aadbfed5017f60801389d08455382dbfc113eeea
Agon Ademaj <codebyagon@gmail.com>
2024-02-17 23:34:54 +0100

    create a serverless api

---
+++ src/db/migrate.ts
@@ -0,0 +1,27 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+import { config } from 'dotenv';
+
+config({
+	path: '.dev.vars',
+});
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql);
+
+const main = async () => {
+	try {
+		await migrate(db, {
+			migrationsFolder: 'drizzle',
+		});
+
+		console.log('Migration Successful');
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+main();
